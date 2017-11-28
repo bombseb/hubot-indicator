73f019a5708ff89814ec40c391c6165de42eab52
Bombseb <bombseb@gmail.com>
2017-11-28 22:57:04 +0400

    Nettoyage du code du fichier hubot-emblems.py

--- nautilus/hubot-emblems.py
+++ nautilus/hubot-emblems.py
@@ -19,68 +19,31 @@
 
 import os
 import sys
-# sys.path.insert(0,"/opt/extras.ubuntu.com/hubot-indicator")
-# sys.path.insert(0,"/media/seb/Données/dev/hubot-indicator")
-
 import urllib
 import dbus
 import urlparse # Python 2.7
 import gi
 gi.require_version('Nautilus', '3.0')
 from gi.repository import Nautilus, GObject
-# from hubotpack.HubicDBus import *
-
-# /home/seb/.local/share/nautilus-python/extensions
-# /home/seb/.icons/hicolor/24x24/emblems
 
 print ("Hubot -> Python v" + sys.version)
 
 
 class HubotProvider(GObject.GObject, Nautilus.InfoProvider):
 	def update_file_info(self, file): #, update_complete, handle):
-		# print ("hubot")
-		# uri = urllib.parse.unquote(file.get_uri()).decode('utf8')
-		# parsed_uri = urllib.parse.urlparse(uri)
-		# if parsed_uri.scheme != 'file': return
-
-		# filepath = os.path.abspath(os.path.join(parsed_uri.netloc, parsed_uri.path))
-
 		uri = urllib.unquote(file.get_uri()).decode('utf8')
 		parsed_uri = urlparse.urlparse(uri)
 		if parsed_uri.scheme != 'file': return
 		filepath = os.path.abspath(os.path.join(parsed_uri.netloc, parsed_uri.path))
 
-		# hubicAccount = HubicAccount ()
-
-		# ret = [u'', True, True]
-
-
-		# format_value = dbus.Array([dbus.Byte(0x14),  # format: 32 bit float
-  #                              dbus.Byte(0x00),  # exponent
-  #                              dbus.Byte(0xAD),  # LSB of unit (%)
-  #                              dbus.Byte(0x27),  # MSB of unit (%)
-  #                              dbus.Byte(0x01),  # namespace
-  #                              dbus.Byte(0x00),  # LSB of description
-  #                              dbus.Byte(0x00)])  # MSB of description
-
-
-		# ret = dbus.Array([dbus.String(""), dbus.Boolean(""), dbus.Boolean("")])
-
-
-		status = ""
-		isPublished = False
-		canBePublished = False
+		# status = ""
+		# isPublished = False
+		# canBePublished = False
 
 		session_bus = dbus.SessionBus()
 		hubic_account_obj = session_bus.get_object('com.hubiC', '/com/hubic/Account')
 		hubic_account_iface = dbus.Interface(hubic_account_obj, 'com.hubic.account')
 		[status, isPublished, canBePublished] = hubic_account_iface.GetItemStatus(filepath)
-
-		# ret = hubicAccount.getItemStatus (filepath)
-		
-		# print (status + "\t" + filepath)
-
-		# status = "Synchronized"
 
 		if status == "Synchronized":
 			file.add_emblem("hubot-synchronized")
@@ -88,16 +51,3 @@ class HubotProvider(GObject.GObject, Nautilus.InfoProvider):
 			file.add_emblem("hubot-syncing")
 		else:
 			return
-
-
-	# com.hubic.account
-	# # GetItemStatus (IN  s     absolutePath, OUT (sbb) ret);
-
-	# Status (string)
-	# The synchronization status. It can be Synchronized (file is ok), Unsynchronized (the file is either downloading or uploading), NoStatus (the file is filtered out or outside of synchronized directory) or else Error when an unexpected exception happen.
-
-	# IsPublished (bool)
-	# Whether the item is published or not.
-
-	# CanBePublished (bool)
-	# Whether the item could be published. It this is false, trying to Publish() the item will certainly fail.
